5b20255869827f79587c9b3c3af497a066fdcfb0
Sharagran <filipmaas@googlemail.com>
2021-05-04 14:35:00 +0200

    public files

--- app.js
+++ app.js
@@ -8,10 +8,14 @@ app.listen(PORT, () => {
     console.log(`app listening at ${PORT}`);
 });
 
-app.get('/', (req, res) => {
+app.get('/', function (req, res) {
     res.send('yoyo');
 });
 
-app.get('*', (req, res) => {
+app.get('/upload', function (req, res) {
+    res.send("upload")
+});
+
+app.get('*', function (req, res) {
     res.send('Error 404 page not found.', 404);
 });
