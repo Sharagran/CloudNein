d42b1659abe67bacb4c6550cf8832b2cbf2eaa39
Sharagran <filipmaas@googlemail.com>
2021-05-05 12:42:56 +0200

    File upload demo & 404 error page

--- app.js
+++ app.js
@@ -1,13 +1,26 @@
 const express = require("express");
 const app = express();
-const PORT = 80;
-var path = require("path");
+const path = require("path");
+const fileUpload = require('express-fileupload'); //https://www.npmjs.com/package/express-fileupload
 
+const PORT = 80;
+
+// Middleware
+app.use(fileUpload({
+    //limits: { fileSize: 50 * 1024 * 1024 }    //upload limit in bytes für alle files
+}));
 app.use(express.static("public"));
 
 app.listen(PORT, () => {
     console.log(`app listening at ${PORT}`);
 });
+
+
+/* TODO: Login
+* validate client login credentials
+* save sessionID on server (database?)
+* send sessionID to client
+*/
 
 app.get('/', function (req, res) {
     res.sendFile(path.join(__dirname + '/authentication/login.html'));
@@ -16,6 +29,21 @@ app.get('/', function (req, res) {
 // https://stackoverflow.com/a/6059938
 app.post('/upload', function (req, res) {
     // upload
+    if(req.files) {
+        let file = req.files.file;
+        //req.query.uploadPath
+        let uploadPath = __dirname + '/UserFiles/' + file.name;
+        
+        file.mv(uploadPath, function(err) {
+            if (err) {
+                return res.status(500).send(err);
+            }
+            res.send('File uploaded!');
+          });
+    } else {
+        res.send("No files uplaoded");
+    }
+    
 });
 
 app.get(['/myfiles', '/myfiles/:path'], function (req, res) {
@@ -25,11 +53,12 @@ app.get(['/myfiles', '/myfiles/:path'], function (req, res) {
     if(path) {
         res.send(path);
     } else {
-        res.send("myfiles");
+        //res.send("myfiles");
+        res.sendFile(__dirname + "/public/upload.html")
     }
     
 })
 
 app.get('*', function (req, res) {
-    res.send('Error 404 page not found.', 404);
+    res.status(404).sendFile(__dirname + '/public/error.html');
 });
