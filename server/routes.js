7c54a59e971381401527ddd88a793bd00755a823
Sharagran <filipmaas@googlemail.com>
2021-05-20 11:32:55 +0200

    auslagern fix

--- server/routes.js
+++ server/routes.js
@@ -1,87 +1,89 @@
 const path = require("path");
 const multer = require('multer');
 const db = require("./Database");
-const { hash_password, compare_hash, sendNewPassword, generatePassword} = require("./Authentication");
+const { hash_password, compare_hash, sendNewPassword, generatePassword } = require("./Authentication");
 const fs = require("fs");
 
-var upload = multer({dest: `${__dirname}/../UserFiles/`});
+var upload = multer({ dest: `${__dirname}/../UserFiles/` });
 
 module.exports = function (app) {
+
 
     app.get('/', function (req, res) {
         res.sendFile(path.join(__dirname + '/public/login.html'));
     });
 
     app.post('/login', (req, res) => {
-        //res.send(`Full name is:${req.body.username} ${req.body.password}.`);
-        db.readData("User", {Username:req.body.username}, (error, result) =>{
-            if(error) {
+        db.readData("User", { Username: req.body.user.username }, (error, result) => {
+            if (error) {
                 throw error;
-            }else if (result.length > 0)
-                compare_hash(req.body.password, result[0].Password, (error, match) => {
-                    if(error) {
+            } else if (result.length > 0)
+                compare_hash(req.body.user.password, result[0].Password, (error, match) => {
+                    if (error) {
                         throw error;
-                    }else {
+                    } else {
                         console.log("Matching password: " + match);
                     };
                 });
-            else{
+            else {
+                console.log(req.body.user.password, req.body.user.username);
                 console.log("Login fehlgeschlagen");
             };
         });
     });
-      
+
     //Verarbeitet die empfangenen Daten beim Registrieren
     app.post('/register', (req, res) => {
         //prüfen, ob Name und Email vorhanden sind, wen nicht dann hashen und speichern
-        console.log(req.body.mail);
-        db.readData("User", {Username:req.body.username, Email:req.body.mail}, (error, result) => {
-            if(error) {
+        console.log(req.body.user.mail);
+        db.readData("User", { Username: req.body.user.username, Email: req.body.user.mail }, (error, result) => {
+            if (error) {
                 throw error;
-            }else if(result.length > 0) {
+            } else if (result.length > 0) {
                 console.log("Username or Email ist already taken");
-            }else {
+            } else {
                 console.log(result);
-                hash_password(req.body.password, (error, hash) => {
-                    if(error) throw error;
-                    db.createData("User", [{Username:req.body.username, Password:hash, Email:req.body.mail}], (error, result) => {
-                        if(error) throw error;
+                hash_password(req.body.user.password, (error, hash) => {
+                    if (error) throw error;
+                    db.createData("User", [{ Username: req.body.user.username, Password: hash, Email: req.body.user.mail }], (error, result) => {
+                        if (error) throw error;
                         console.log(result);
-                    }); 
+                    });
                 });
             };
         });
     });
-    
+
     //Erzeugt ein neues Passwort, updatet dies in der DB und sendet eine Mail an den User
-    app.post('/forgotPassword', (req, res) =>{
-        db.readData('User', {Email:req.body.email}, (error, result) => {
-            if(error) {
+    app.post('/forgotPassword', (req, res) => {
+        console.log(req.body.email.mail);
+        db.readData('User', { Email: req.body.email.email }, (error, result) => {
+            if (error) {
                 throw error;
-            }else if(result.length < 1) {
+            } else if (result.length < 1) {
                 console.log("Email nicht gefunden");
-            }else {
+            } else {
                 newPassword = generatePassword();
-                hash_password(newPassword, (error,hash) =>{
-                    if(error) throw error;
-                    db.updateData("User",{Email:req.body.email}, { $set:{ Password:hash}}, (error,result) => {
-                        if(error) throw error;
+                hash_password(newPassword, (error, hash) => {
+                    if (error) throw error;
+                    db.updateData("User", { Email: req.body.email.email }, { $set: { Password: hash } }, (error, result) => {
+                        if (error) throw error;
                         console.log(result);
                     })
-                    sendNewPassword(result[0].Email, newPassword, (error, info) =>{
-                        if(error) throw error;
+                    sendNewPassword(result[0].Email, newPassword, (error, info) => {
+                        if (error) throw error;
                     });
                 })
             };
         });
     });
-    
+
     app.get('/dbtest', function (req, res) {
-        db.readData("test", {value1:"test1"}, (error, result) => {
+        db.readData("test", { value1: "test1" }, (error, result) => {
             console.log(result);
         });
     });
-    
+
     // https://stackoverflow.com/a/6059938
     app.get('/upload', function (req, res) {
         res.sendFile(__dirname + "/public/upload.html");
@@ -89,9 +91,9 @@ module.exports = function (app) {
     app.post('/upload', upload.array("files"), function (req, res) {
         for (const key in req.files) {
             const file = req.files[key];
-    
-            fs.rename(file.path, file.destination+file.originalname, function (error) {
-                if(error)
+
+            fs.rename(file.path, file.destination + file.originalname, function (error) {
+                if (error)
                     throw error;
             });
         }
@@ -100,23 +102,23 @@ module.exports = function (app) {
         };
         res.end(JSON.stringify(responseJSON));
     });
-    
+
+
     app.get(['/myfiles', '/myfiles/:path'], function (req, res) {
         // download
         var path = req.params.path;
-    
-        if(path) {
+
+        if (path) {
             res.send(path);
         } else {
             res.send("myfiles");
         }
     });
-    
+
     app.get('/files/:id', function (req, res) {
-        
+
     });
-    
-    // Error route must be the last
+
     app.get('*', function (req, res) {
         res.status(404).sendFile(__dirname + '/public/error.html');
     });
