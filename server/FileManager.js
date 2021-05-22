64ba89f1636ce2bb626590feb3dea4885980bf39
Sharagran <filipmaas@googlemail.com>
2021-05-22 11:44:27 +0200

    fileManager doc

--- server/FileManager.js
+++ server/FileManager.js
@@ -1,4 +1,5 @@
 const fs = require("fs");
+const db = require("./Database");
 
 function uploadFiles(req, userID) {
     for (const key in req.files) {
@@ -22,7 +23,41 @@ function uploadFiles(req, userID) {
 }
 
 function getFiles(userID) {
-    
+    throw {name : "NotImplementedError", message : "too lazy to implement"};
+
+    //TODO: get all user files
+}
+
+function commentFile() {
+    throw {name : "NotImplementedError", message : "too lazy to implement"};
+
+    //TODO: add new comment db entry
+}
+
+function editFile(file, newContent) {
+    throw {name : "NotImplementedError", message : "too lazy to implement"};
+
+    fs.writeFile(file, newContent, function (error) {
+        
+    });
+}
+
+function moveFile(oldPath, newPath) {
+    throw {name : "NotImplementedError", message : "too lazy to implement"};
+
+    fs.rename(oldPath, newPath, function (error) {
+        if(error)
+            throw error;
+
+        //TODO: update file metadata in db
+
+    });
+}
+
+function shareFile() {
+    throw {name : "NotImplementedError", message : "too lazy to implement"};
+
+    //TODO: modify file access & create share link
 }
 
 module.exports = {
