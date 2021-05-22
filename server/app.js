0c529727b1878b7c4e2273c80f89fa8bed8fe152
Sharagran <filipmaas@googlemail.com>
2021-05-22 12:24:21 +0200

    routing improved

--- server/app.js
+++ server/app.js
@@ -5,8 +5,6 @@ const parser = require("body-parser");
 const cors = require("cors");
 const routes = require('./routes')
 
-
-
 const PORT = 80;
 
 
@@ -15,11 +13,19 @@ app.use(express.static("public"));
 app.use(cors());
 app.use(parser.urlencoded({ extended: false })); 
 app.use(express.json())
+
+app.use(routes);
+
+// handles all code errors (error middleware must be the last middleware)
+app.use(function (err, req, res, next) {
+    console.error(err.stack)
+    res.status(500);
+    res.send('Something broke!');
+    //next(err) // falls keine response gesendet wird kümmert sich express darum mit next(error)
+});
+
+
 app.listen(PORT, () => {
     console.log(`app listening at ${PORT}`);
     db.connect();
 });
-
-// FIXME: ersetzen durch router mit app.use(routes)
-routes(app);
-
--- server/routes.js
+++ server/routes.js
@@ -3,114 +3,119 @@ const multer = require('multer');
 const db = require("./Database");
 const { hash_password, compare_hash, sendNewPassword, generatePassword } = require("./Authentication");
 const fm = require('./FileManager');
+const express = require("express");
 
 var upload = multer({ dest: `${__dirname}/../UserFiles/` });
 
-module.exports = function (app) {
+var router = express.Router();
 
-    app.post('/login', (req, res) => {
-        db.readData("User", { Username: req.body.user.username }, (error, result) => {
-            if (error) {
-                throw error;
-            } else if (result.length > 0)
-                compare_hash(req.body.user.password, result[0].Password, (error, match) => {
-                    if (error) {
-                        throw error;
-                    } else {
-                        console.log("Matching password: " + match);
-                    };
-                });
-            else {
-                console.log(req.body.user.password, req.body.user.username);
-                console.log("Login fehlgeschlagen");
-            };
-        });
+router.post('/login', (req, res) => {
+    db.readData("User", { Username: req.body.user.username }, (error, result) => {
+        if (error) {
+            throw error;
+        } else if (result.length > 0)
+            compare_hash(req.body.user.password, result[0].Password, (error, match) => {
+                if (error) {
+                    throw error;
+                } else {
+                    console.log("Matching password: " + match);
+                };
+            });
+        else {
+            console.log(req.body.user.password, req.body.user.username);
+            console.log("Login fehlgeschlagen");
+        };
     });
+});
 
-    //Verarbeitet die empfangenen Daten beim Registrieren
-    app.post('/register', (req, res) => {
-        //prüfen, ob Name und Email vorhanden sind, wen nicht dann hashen und speichern
-        console.log(req.body.user.mail);
-        db.readData("User", { Username: req.body.user.username, Email: req.body.user.mail }, (error, result) => {
-            if (error) {
-                throw error;
-            } else if (result.length > 0) {
-                console.log("Username or Email ist already taken");
-            } else {
-                console.log(result);
-                hash_password(req.body.user.password, (error, hash) => {
-                    if (error) throw error;
-                    db.createData("User", [{ Username: req.body.user.username, Password: hash, Email: req.body.user.mail }], (error, result) => {
-                        if (error) throw error;
-                        console.log(result);
-                    });
-                });
-            };
-        });
-    });
-
-    //Erzeugt ein neues Passwort, updatet dies in der DB und sendet eine Mail an den User
-    app.post('/forgotPassword', (req, res) => {
-        console.log(req.body.email.mail);
-        db.readData('User', { Email: req.body.email.email }, (error, result) => {
-            if (error) {
-                throw error;
-            } else if (result.length < 1) {
-                console.log("Email nicht gefunden");
-            } else {
-                newPassword = generatePassword();
-                hash_password(newPassword, (error, hash) => {
-                    if (error) throw error;
-                    db.updateData("User", { Email: req.body.email.email }, { $set: { Password: hash } }, (error, result) => {
-                        if (error) throw error;
-                        console.log(result);
-                    })
-                    sendNewPassword(result[0].Email, newPassword, (error, info) => {
-                        if (error) throw error;
-                    });
-                })
-            };
-        });
-    });
-
-    app.post('/upload', upload.array("files"), function (req, res) {
-        var responseJSON = fm.uploadFiles(req);
-        res.end(JSON.stringify(responseJSON));
-    });
-
-
-    // FIXME: Debug only (all get routes should be handled with react)
-    app.get('/', function (req, res) {
-        res.sendFile(path.join(__dirname + '/public/login.html'));
-    });
-
-    app.get('/dbtest', function (req, res) {
-        db.readData("test", { value1: "test1" }, (error, result) => {
-            console.log(result);
-        });
-    });
-
-    app.get('/upload', function (req, res) {
-        res.sendFile(__dirname + "/public/upload.html");
-    });
-
-    app.get(['/myfiles', '/myfiles/:path'], function (req, res) {
-        // download
-        var path = req.params.path;
-
-        if (path) {
-            res.send(path);
+//Verarbeitet die empfangenen Daten beim Registrieren
+router.post('/register', (req, res) => {
+    //prüfen, ob Name und Email vorhanden sind, wen nicht dann hashen und speichern
+    console.log(req.body.user.mail);
+    db.readData("User", { Username: req.body.user.username, Email: req.body.user.mail }, (error, result) => {
+        if (error) {
+            throw error;
+        } else if (result.length > 0) {
+            console.log("Username or Email ist already taken");
         } else {
-            res.send("myfiles");
-        }
+            console.log(result);
+            hash_password(req.body.user.password, (error, hash) => {
+                if (error) throw error;
+                db.createData("User", [{ Username: req.body.user.username, Password: hash, Email: req.body.user.mail }], (error, result) => {
+                    if (error) throw error;
+                    console.log(result);
+                });
+            });
+        };
     });
+});
 
-    app.get('/files/:id', function (req, res) {
-
+//Erzeugt ein neues Passwort, updatet dies in der DB und sendet eine Mail an den User
+router.post('/forgotPassword', (req, res) => {
+    console.log(req.body.email.mail);
+    db.readData('User', { Email: req.body.email.email }, (error, result) => {
+        if (error) {
+            throw error;
+        } else if (result.length < 1) {
+            console.log("Email nicht gefunden");
+        } else {
+            newPassword = generatePassword();
+            hash_password(newPassword, (error, hash) => {
+                if (error) throw error;
+                db.updateData("User", { Email: req.body.email.email }, { $set: { Password: hash } }, (error, result) => {
+                    if (error) throw error;
+                    console.log(result);
+                })
+                sendNewPassword(result[0].Email, newPassword, (error, info) => {
+                    if (error) throw error;
+                });
+            })
+        };
     });
+});
 
-    app.get('*', function (req, res) {
-        res.status(404).sendFile(__dirname + '/public/error.html');
+router.post('/upload', upload.array("files"), function (req, res) {
+    var responseJSON = fm.uploadFiles(req);
+    res.end(JSON.stringify(responseJSON));
+});
+
+
+// FIXME: Debug only (all get routes should be handled with react)
+router.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname + '/public/login.html'));
+});
+
+router.get('/dbtest', function (req, res) {
+    db.readData("test", { value1: "test1" }, (error, result) => {
+        console.log(result);
     });
+});
 
-}
+router.get('/upload', function (req, res) {
+    res.sendFile(__dirname + "/public/upload.html");
+});
+
+router.get(['/myfiles', '/myfiles/:path'], function (req, res) {
+    // download
+    var path = req.params.path;
+
+    if (path) {
+        res.send(path);
+    } else {
+        res.send("myfiles");
+    }
+});
+
+router.get('/files/:id', function (req, res) {
+
+});
+
+router.get('/error', function (req, res) {
+    fghfghfg.error();
+})
+
+router.get('*', function (req, res) {
+    res.status(404).sendFile(__dirname + '/public/error.html');
+});
+
+module.exports = router;
