c7e77b8f763009c12edbe174d9f145832f9e7ed5
IsaakDl <Daniel.Isaak@pm.me>
2021-05-25 13:53:11 +0200

    Recording and Photo

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,8 @@ import Registration from './pages/Registration';
 import ForgotPassword from './pages/ForgotPassword';
 import Home from './pages/Home';
 import Settings from './pages/Settings';
+import Record from './pages/Record';
+import Photo from './pages/Photo';
 
 function App() {
   return (
@@ -14,6 +16,8 @@ function App() {
         <Route path="/ForgotPassword" component={ForgotPassword} />
         <Route path="/Home" component={Home} />
         <Route path="/Settings" component={Settings} />
+        <Route path="/Record" component={Record} />
+        <Route path="/Photo" component={Photo} />
       </Switch>
     </BrowserRouter>
 
---
+++ client/src/pages/Record.js
@@ -0,0 +1,63 @@
+import React, { Component } from "react";
+import {Recorder} from 'react-voice-recorder'
+import './css/recorder.css'
+
+export default class Record extends Component {
+
+    constructor(props) {
+        super(props)
+    }
+
+    state = {
+        audioDetails: {
+            url: null,
+            blob: null,
+            chunks: null,
+            duration: {
+              h: 0,
+              m: 0,
+              s: 0
+            }
+          }
+    }
+
+    handleAudioStop(data){
+        console.log(data)
+        this.setState({ audioDetails: data });
+    }
+
+    handleAudioUpload(file) {
+        console.log(file); //FIXME: Do stuff with audio file.
+    }
+
+    handleReset() {
+        const reset = {
+          url: null,
+          blob: null,
+          chunks: null,
+          duration: {
+            h: 0,
+            m: 0,
+            s: 0
+          }
+        };
+        this.setState({ audioDetails: reset });
+    }
+
+    render() {
+        return (
+            <>
+                <Recorder
+                    record={true}
+                    title={"New recording"}
+                    audioURL={this.state.audioDetails.url}
+                    showUIAudio
+                    handleAudioStop={data => this.handleAudioStop(data)}
+                    handleAudioUpload={data => this.handleAudioUpload(data)}
+                    handleReset={() => this.handleReset()}
+                />
+            </>
+        );
+        
+    }
+}
