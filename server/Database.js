26a0f16aa9deed6e66609cb38b881d3d9fb01c07
Sharagran <filipmaas@googlemail.com>
2021-06-01 12:42:21 +0200

    Database promises(sync)

--- server/Database.js
+++ server/Database.js
@@ -1,54 +1,90 @@
 const mongo = require("mongodb").MongoClient;
+const util = require('util');
 let db;
 
-exports.connect = () => {
+const create = util.promisify(createData);
+const read = util.promisify(readData);
+const update = util.promisify(updateData);
+const _delete = util.promisify(deleteData);
+
+function connect () {
     mongo.connect("mongodb://localhost:27017/", {useNewUrlParser: true, useUnifiedTopology: true}, (error, client) => {
         if(error) throw error;
         db = client.db("cloudnein");
     });
 }
 
-exports.createData = (collectionName, data, callback = defaultCallBack) => {
+function createData (collectionName, data, callback = defaultCallBack) {
     var collection = db.collection(collectionName);
     if (!Array.isArray(data)) {
         collection.insertOne(data, (error, result) => {
             if (error) throw error;
-            callback(error, result);
+            callback(result);
         });
     }
     else {
         collection.insertMany(data, (error, result) => {
             if (error) throw error;
-            callback(error, result);
+            callback(result);
         });
     }
 }
 
-exports.readData = (collectionName, query = {}, callback = defaultCallBack, limit = 0, sort = {}) => {
+function readData (collectionName, query = {}, callback = defaultCallBack, limit = 0, sort = {}) {
     collection = db.collection(collectionName);
     collection.find(query).limit(0).sort(sort).toArray(function (error, result) {
         if (error) throw error;
-        callback(error, result);
+        callback(result);
     });
 }
 
-exports.updateData = (collectionName, query = {}, values = {}, callback = defaultCallBack) => {
+function updateData (collectionName, query = {}, values = {}, callback = defaultCallBack) {
     collection = db.collection(collectionName);
     collection.updateMany(query, values, (error, result) => {
         if (error) throw error;
-        callback(error, result)
+        callback(result)
     });
 }
 
-exports.deleteData = (collectionName, query = {}, callback = defaultCallBack) => {
+function deleteData (collectionName, query = {}, callback = defaultCallBack) {
     collection = db.collection(collectionName);
     collection.deleteMany(query, (error, result) => {
         if (error) throw error;
-        callback(error,result);
+        callback(result);
     });
 }
 
-function defaultCallBack(error, result) {
-    if(error) throw error;
+function defaultCallBack(result) {
     console.log("default callback: " + result);
 }
+
+async function createDataSync(collectionName, data) {
+    return await create(collectionName, data);
+}
+
+async function readDataSync(collectionName, query = {}) {
+    return await read(collectionName, query);
+}
+
+async function updateDataSync(collectionName, query = {}, values = {}) {
+    return await update(collectionName, query, values);
+}
+
+async function deleteDataSync(collectionName, query = {}) {
+    return await deleteData(collectionName, query);
+}
+
+module.exports = {
+    connect: connect,
+
+    createData: createData,
+    readData: readData,
+    updateData: updateData,
+    deleteData: deleteData,
+
+    createDataSync: createDataSync,
+    readDataSync: readDataSync,
+    updateDataSync: updateDataSync,
+    deleteDataSync: deleteDataSync
+}
+
