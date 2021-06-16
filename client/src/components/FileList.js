573f6b9a7591e54da1e54c2fdb31e346fac8562f
Sharagran <filipmaas@googlemail.com>
2021-06-16 18:06:21 +0200

    share file UI preperation

--- client/src/components/FileList.js
+++ client/src/components/FileList.js
@@ -2,10 +2,10 @@ import React from 'react'
 import File from './File';
 
 
-export default function FileList({ files, clickHandler }) {
+export default function FileList({ files }) {
     return (
         files.map(file => {
-            return <File key={file.id} name={file.name} isFolder={file.isFolder} clickHandler={clickHandler} />
+            return <File key={file.id} name={file.name} isFolder={file.isFolder} />
         })
     )
 }
